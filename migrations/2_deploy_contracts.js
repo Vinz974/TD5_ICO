a128b9b4dcf99231aa4a84bee52597bad2a2b2eb
Vincent <vincent.moutel@edu.devinci.fr>
2020-03-02 16:34:12 +0100

    changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,8 +7,8 @@ var Owner=artifacts.require("Owner");
 var Secondary=artifacts.require("Secondary");
 
 module.exports = function(deployer) {
-  deployer.deploy(MetaCoin,arg="d_value", coderType="uint256",value=10000);
   deployer.deploy(Owner,arg="d_value", coderType="uint256",value=10000);
   deployer.deploy(Secondary,arg="d_value", coderType="uint256",value=10000);
+  deployer.deploy(MetaCoin,arg="d_value", coderType="uint256",value=10000);
   deployer.deploy(ERC20,arg="d_value", coderType="uint256",value=10000);
 };
