d5cda641c59d1a40df3221fedc4b4b4d1f077da4
Vincent <vincent.moutel@edu.devinci.fr>
2020-03-02 21:38:10 +0100

    Add whitelist

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,10 @@
 var ERC20 = artifacts.require("ERC20");
 var MetaCoin=artifacts.require("MetaCoin");
-var Context=artifacts.require("Context");
-var SafeMath=artifacts.require("SafeMath");
-var IERC20=artifacts.require("IERC20");
-var Owner=artifacts.require("Owner");
+var Ownable=artifacts.require("Ownable");
 var Secondary=artifacts.require("Secondary");
 
 module.exports = function(deployer) {
-  deployer.deploy(Owner,arg="d_value", coderType="uint256",value=10000);
+  deployer.deploy(Ownable,arg="d_value", coderType="uint256",value=10000);
   deployer.deploy(Secondary,arg="d_value", coderType="uint256",value=10000);
   deployer.deploy(MetaCoin,arg="d_value", coderType="uint256",value=10000);
   deployer.deploy(ERC20,arg="d_value", coderType="uint256",value=10000);
